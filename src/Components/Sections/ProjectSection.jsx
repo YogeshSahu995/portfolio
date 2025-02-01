c0c6a524b5ae74ec265162345a57f4c1c13aed05
YogeshKumar995 <1413002yogesh10206@gmail.com>
2025-02-01 20:45:05 +0530

    adjustment in css

--- src/Components/Sections/ProjectSection.jsx
+++ src/Components/Sections/ProjectSection.jsx
@@ -4,35 +4,35 @@ export function ProjectSection() {
     const slides = [
         {
             imageurl: './projectImages/YouFeed.png',
-            name : 'You Page'
+            name: 'You Page'
         },
         {
             imageurl: './projectImages/yourChannel.png',
-            name : 'Your Channel'
+            name: 'Your Channel'
         },
         {
             imageurl: './projectImages/channelVideos.png',
-            name : 'Channel Videos'
+            name: 'Channel Videos'
         },
         {
             imageurl: './projectImages/channelPosts.png',
-            name : 'Channel Posts'
+            name: 'Channel Posts'
         },
         {
             imageurl: './projectImages/openPlaylist.png',
-            name : 'Opened Playlist'
+            name: 'Opened Playlist'
         },
         {
             imageurl: './projectImages/playVideo.png',
-            name : 'Play video'
+            name: 'Play video'
         },
         {
             imageurl: './projectImages/videoComment.png',
-            name : 'Video comments'
+            name: 'Video comments'
         },
         {
             imageurl: './projectImages/subscribedChannelInfo.png',
-            name : 'Subscribed Channel'
+            name: 'Subscribed Channel'
         },
         {
             imageurl: './projectImages/customizeForm.png',
@@ -59,22 +59,22 @@ export function ProjectSection() {
                         <Carousel>
                             {
                                 slides.map((e) => (
-                                    <img src={e.imageurl} />
+                                    <img src={e.imageurl} key={e.name}/>
                                 ))
                             }
                         </Carousel>
-                            <div className="flex justify-between w-[50vw] mx-auto my-4">
-                                <Button 
-                                    value="Demo"
-                                    icon={<i class="ri-arrow-right-up-line"></i>}
-                                />
-                                <Button 
-                                    value="Source"
-                                    icon={<i class="ri-arrow-right-up-line"></i>}
-                                />
-                            </div>
-                        <div className="flex flex-col items-center gap-3 mt-3 border p-2 rounded bg-[#00000049]">
-                            <div className="flex gap-4">
+                        <div className="flex justify-between w-full md:w-[50vw] mx-auto my-4">
+                            <Button
+                                value="Demo"
+                                icon={<i className="ri-arrow-right-up-line"></i>}
+                            />
+                            <Button
+                                value="Source"
+                                icon={<i className="ri-arrow-right-up-line"></i>}
+                            />
+                        </div>
+                        <div className="flex flex-col items-center justify-around gap-3 mt-3 border p-2 rounded bg-[#00000049]">
+                            <div className="flex gap-4 flex-wrap justify-center items-center">
                                 <p className="tech">React</p>
                                 <p className="tech">Express</p>
                                 <p className="tech">Node</p>
@@ -82,7 +82,7 @@ export function ProjectSection() {
                                 <p className="tech">TailwindCss</p>
                             </div>
                             <p>
-                            Through my YouTube Clone project, I have gained valuable experience in various key aspects of web development. I learned how to make API calls efficiently, implement authentication using access and refresh tokens, and manage token refreshing with Axios interceptors. Additionally, I explored how to send form data to the backend, design and develop APIs, and implement pagination and debouncing for better performance. I also worked with API query parameters to enable search functionality and integrated third-party services like Cloudinary for media management. Moreover, I deepened my understanding of middleware, utilizing it effectively for authentication, request handling, and other backend operations. Throughout the development process, I extensively used Postman to test and debug APIs, ensuring seamless communication between the frontend and backend.
+                                Through my YouTube Clone project, I have gained valuable experience in various key aspects of web development. I learned how to make API calls efficiently, implement authentication using access and refresh tokens, and manage token refreshing with Axios interceptors. Additionally, I explored how to send form data to the backend, design and develop APIs, and implement pagination and debouncing for better performance. I also worked with API query parameters to enable search functionality and integrated third-party services like Cloudinary for media management. Moreover, I deepened my understanding of middleware, utilizing it effectively for authentication, request handling, and other backend operations. Throughout the development process, I extensively used Postman to test and debug APIs, ensuring seamless communication between the frontend and backend.
                             </p>
                         </div>
                     </div>
